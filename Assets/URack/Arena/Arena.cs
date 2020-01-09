d9e8834737d820efccee7a03ae194e5658cff6cc
Matthew Hughes <13041191@adsroot.uts.edu.au>
2020-01-09 13:52:39 +1100

    Added Sky parameters to Arena

--- Assets/URack/Arena/Arena.cs
+++ Assets/URack/Arena/Arena.cs
@@ -8,6 +8,18 @@ namespace Eidetic.URack
 {
     public class Arena : UModule
     {
+
+        //----------------------------------
+        // Constants
+        //----------------------------------
+        static readonly Color[] Monochromatic = new Color[] { Color.HSVToRGB(0, .7f, 1), Color.HSVToRGB(0, 1, 1), Color.HSVToRGB(0, .7f, .5f) };
+        static readonly Color[] Analogous = new Color[] { Color.HSVToRGB(0.05f, .95f, 1), Color.HSVToRGB(0, 1, 1), Color.HSVToRGB(0.83333f, .95f, 1f) };
+        static readonly Color[] Triadic = new Color[] { Color.HSVToRGB(0.16388f, .9f, 1), Color.HSVToRGB(0, 1, 1), Color.HSVToRGB(0.56388f, .95f, 1f) };
+        static readonly Color[] Complementary = new Color[] { Color.HSVToRGB(0.33888f, 1f, 1), Color.HSVToRGB(0, 1, 1), Color.HSVToRGB(0.42222f, .95f, 1f) };
+
+        //----------------------------------
+        // Prefab objects/components
+        //----------------------------------
         [SerializeField] Camera Camera;
         [SerializeField] GameObject CameraOrigin;
         [SerializeField] GameObject ForwardAxis;
@@ -15,16 +27,19 @@ namespace Eidetic.URack
         [SerializeField] GameObject UpAxis;
         [SerializeField] GameObject DownAxis;
         [SerializeField] Volume PostProcessing;
+        [SerializeField] Volume Sky;
         [SerializeField] GameObject OriginMarker;
 
-        Exposure ExposureComponent;
-        void Start()
-        {
-            Exposure exposure;
-            if (PostProcessing.profile.TryGet<Exposure>(out exposure))
-                ExposureComponent = exposure;
-        }
+        //----------------------------------
+        // Private variables
+        //----------------------------------
+        Exposure LightExposureComponent;
+        GradientSky GradientSkyComponent;
+        Vector3 Orbit = Vector3.zero;
 
+        //----------------------------------
+        // Setters for applying voltages
+        //----------------------------------
         public float CameraOriginX
         {
             set => CameraOrigin.transform.position = CameraOrigin.transform.position.Replace(0, value.Map(-10, 10));
@@ -37,7 +52,6 @@ namespace Eidetic.URack
         {
             set => CameraOrigin.transform.position = CameraOrigin.transform.position.Replace(2, value.Map(-10, 10));
         }
-
         float cameraDistance;
         public float CameraDistance
         {
@@ -50,51 +64,110 @@ namespace Eidetic.URack
                     Camera.transform.position = Vector3.MoveTowards(Camera.transform.position, BackwardAxis.transform.position, 0.001f);
             }
         }
-
-        Vector3 orbit = new Vector3();
-
         float cameraOrbitX;
         public float CameraOrbitX
         {
             set
             {
                 var newValue = value.Map(-180f, 180f);
-                orbit = orbit.Replace(1, newValue);
+                Orbit = Orbit.Replace(1, newValue);
                 var diff = cameraOrbitX - newValue;
                 // CameraOrigin.transform.Rotate(0, diff, 0);
                 cameraOrbitX = newValue;
             }
         }
-
         float cameraOrbitY;
         public float CameraOrbitY
         {
             set
             {
                 var newValue = value.Clamp().Map(89.99f, -89.99f);
-                orbit = orbit.Replace(0, newValue);
+                Orbit = Orbit.Replace(0, newValue);
                 var diff = cameraOrbitY - newValue;
                 // CameraOrigin.transform.Rotate(diff, 0, 0);
                 cameraOrbitY = newValue;
             }
         }
-
-        void Update()
-        {
-            CameraOrigin.transform.SetPositionAndRotation(CameraOrigin.transform.position, Quaternion.Euler(orbit));
-        }
-
         public float CameraFocalLength
         {
             set => Camera.focalLength = value.Clamp(0, 10).Map(0, 10, 8, 180);
         }
-        public float Exposure
+        public float LightExposure
         {
-            set => ExposureComponent.fixedExposure.value = value.Map(0, 20);
+            set => LightExposureComponent.fixedExposure.value = value.Map(0, 20);
         }
         public bool MarkerEnable
         {
             set => OriginMarker.SetActive(value);
+        }
+        public float SkyColorHarmony { get; set; }
+        public float SkyColorPhase { get; set; }
+        public float SkyColorDiffusion { get; set; }
+        public float SkyExposure { get; set; }
+
+        //----------------------------------
+        // Unity magic methods
+        //----------------------------------
+        void Start()
+        {
+            Exposure exposure;
+            if (PostProcessing.profile.TryGet<Exposure>(out exposure))
+                LightExposureComponent = exposure;
+            GradientSky gradient;
+            if (Sky.profile.TryGet<GradientSky>(out gradient))
+                GradientSkyComponent = gradient;
+        }
+
+        void Update()
+        {
+            // perform the orbital camera rotaiton
+            CameraOrigin.transform.SetPositionAndRotation(CameraOrigin.transform.position, Quaternion.Euler(Orbit));
+
+            // Set the Sky
+            
+            // Calculate the base harmonies
+            var harmony = SkyColorHarmony.Clamp(0, 10).Map(0, 10, 0, 1);
+            Color topColor, midColor, bottomColor;
+            if (harmony <= (1f / 3f))
+            {
+                var lerp = harmony / (1f / 3f);
+                topColor = Color.Lerp(Monochromatic[0], Analogous[0], lerp);
+                midColor = Color.Lerp(Monochromatic[1], Analogous[1], lerp);
+                bottomColor = Color.Lerp(Monochromatic[2], Analogous[2], lerp);
+            }
+            else if (harmony <= (2f / 3f))
+            {
+                var lerp = (harmony - (1f / 3f)) / (1f / 3f);
+                topColor = Color.Lerp(Analogous[0], Triadic[0], lerp);
+                midColor = Color.Lerp(Analogous[1], Triadic[1], lerp);
+                bottomColor = Color.Lerp(Analogous[2], Triadic[2], lerp);
+            }
+            else
+            {
+                var lerp = (harmony - (2f / 3f)) / (1f / 3f);
+                topColor = Color.Lerp(Triadic[0], Complementary[0], lerp);
+                midColor = Color.Lerp(Triadic[1], Complementary[1], lerp);
+                bottomColor = Color.Lerp(Triadic[2], Complementary[2], lerp);
+            }
+
+            // Apply offsets
+            Color.RGBToHSV(topColor, out float topHue, out float topSat, out float topVal);
+            Color.RGBToHSV(midColor, out float midHue, out float midSat, out float midVal);
+            Color.RGBToHSV(bottomColor, out float bottomHue, out float bottomSat, out float bottomVal);
+
+            var phase = SkyColorPhase.Map(0, 1);
+            if (phase < 0) phase = Mathf.CeilToInt(Mathf.Abs(phase)) - phase;
+            topHue = (topHue + phase) % 1;
+            midHue = (midHue + phase) % 1;
+            bottomHue = (bottomHue + phase) % 1;
+
+            // Set parameters
+            GradientSkyComponent.top.value = Color.HSVToRGB(topHue, topSat, topVal);
+            GradientSkyComponent.middle.value = Color.HSVToRGB(midHue, midSat, midVal);
+            GradientSkyComponent.bottom.value = Color.HSVToRGB(bottomHue, bottomSat, bottomVal);
+
+            GradientSkyComponent.gradientDiffusion.value = SkyColorDiffusion.Clamp(0, 10).Map(0, 10, 0, 10, 3);
+            GradientSkyComponent.exposure.value = SkyExposure.Clamp(0, 10).Map(0, 10, 0, 20);
         }
     }
 
