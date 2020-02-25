b9dbeb0230e8547b118f8dde5dfaeb1ebbde054f
Matt Hughes <matth@FEIT013085.local>
2020-02-25 16:05:14 +1100

    update

--- Runtime/Modules/Drone/Drone.cs
+++ Runtime/Modules/Drone/Drone.cs
@@ -9,67 +9,74 @@ namespace Eidetic.URack
 {
     public class Drone : UModule
     {
-		[Input(-5, 5, -10, 10)]
-		public float Pace { get; set; }
+		
+	[Input(-5, 5, -10, 10)]
+	public float X
+	{
+		set => CameraOrigin.transform.position = CameraOrigin.transform.position.Replace(0, value);
+	}
 
-		[Input(-5, 5, -10, 10)]
-		public float Strafe { get; set; }
+	[Input(-5, 5, -10, 10)]
+	public float Y
+	{
+		set => CameraOrigin.transform.position = CameraOrigin.transform.position.Replace(1, value);
+	}
+
+	[Input(-5, 5, -10, 10)]
+	public float Z
+	{
+		set => CameraOrigin.transform.position = CameraOrigin.transform.position.Replace(2, value);
+	}
+
+        Vector3 orbit = Vector3.zero;
+        [Input(-5, 5, -180, 180, false, 1, 1)]
+        public float Orbit
+        {
+            set => orbit = orbit.Replace(1, value);
+        }
+
+        [Input(-5, 5, -90, 90, false, 1, 1)]
+        public float Elevation
+        {
+            set => orbit = orbit.Replace(0, value);
+        }
 
         float distance;
         [Input]
         public float Distance
         {
+	    get => distance;
             set
             {
-                distance = value.Clamp(0.01f, 10);
-                while (Vector3.Distance(Camera.transform.position, CameraOrigin.transform.position) > distance + 0.01f)
-                    Camera.transform.position = Vector3.MoveTowards(Camera.transform.position, ForwardAxis.transform.position, 0.0005f);
-                while (Vector3.Distance(Camera.transform.position, CameraOrigin.transform.position) < distance - 0.01f)
-                    Camera.transform.position = Vector3.MoveTowards(Camera.transform.position, BackwardAxis.transform.position, 0.0005f);
+                distance = value > 0.1f ? value : 0.1f;
             }
         }
-        [Input(-5, 5, -180, 180, false, 1, 1)]
-        public float OrbitX
-        {
-            set => Orbit = Orbit.Replace(1, value);
-        }
-        [Input(-5, 5, -180, 180, false, 1, 1)]
-        public float OrbitY
-        {
-            set => Orbit = Orbit.Replace(0, value);
-        }
-        [Input]
-        public float Zoom
-        {
-            set => Camera.focalLength = value.Clamp(0, 10).Map(0, 10, 8, 180);
-        }
+
+	public int Target
+	{
+		set => OriginTarget.SetActive(value > 0);
+	}
 
         [SerializeField] Camera Camera;
         [SerializeField] GameObject CameraOrigin;
         [SerializeField] GameObject ForwardAxis;
         [SerializeField] GameObject BackwardAxis;
-        [SerializeField] GameObject LeftAxis;
-        [SerializeField] GameObject RightAxis;
-        [SerializeField] GameObject UpAxis;
-        [SerializeField] GameObject DownAxis;
-        [SerializeField] Volume PostProcessing;
-
-        Vector3 Orbit = Vector3.zero;
-
-        void Start() { }
+        [SerializeField] GameObject OriginTarget;
+	[SerializeField] Volume PostProcessing;
 
         public void Update()
         {
-			var position = CameraOrigin.transform.position;
-			// apply movement
-			if (Pace != 0)
-				position += CameraOrigin.transform.TransformDirection(Vector3.forward) * Pace * Time.deltaTime;
-			if (Strafe != 0)
-				position += CameraOrigin.transform.TransformDirection(Vector3.right) * Strafe * Time.deltaTime;
-
             // perform the orbital camera rotaiton
-            CameraOrigin.transform.SetPositionAndRotation(position, Quaternion.Euler(Orbit));
-		}
+            CameraOrigin.transform.SetPositionAndRotation(CameraOrigin.transform.position, Quaternion.Euler(orbit));
+	    // distance movement                
+	    if (Vector3.Distance(Camera.transform.position, CameraOrigin.transform.position) > distance + 0.01f){
+	    while (Vector3.Distance(Camera.transform.position, CameraOrigin.transform.position) > distance + 0.005f)
+                Camera.transform.position = Vector3.MoveTowards(Camera.transform.position, ForwardAxis.transform.position, 0.0005f);
+} else if (Vector3.Distance(Camera.transform.position, CameraOrigin.transform.position) < distance - 0.01f) {
+            while (Vector3.Distance(Camera.transform.position, CameraOrigin.transform.position) < distance - 0.005f)
+                Camera.transform.position = Vector3.MoveTowards(Camera.transform.position, BackwardAxis.transform.position, 0.0005f);
+}
+	}
     }
 
 }
