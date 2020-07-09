12fc21ac4aa4d2957934e43c078a7d00588b5fcf
Matthew Hughes <matt.hughes@live.com>
2020-07-09 13:07:15 +1000

    Added exporter tool for packaging URack plugins

--- Editor/PluginExporter/ExporterWindow.cs
+++ Editor/PluginExporter/ExporterWindow.cs
@@ -1,46 +1,68 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using UnityEditor;
 using System.Diagnostics;
 using System.IO;
+using UnityEditor;
+using UnityEngine;
+using LightBuzz.Archiver;
 
 public class ExporterWindow : EditorWindow
 {
-    string PluginName = "Plugin Name";
-    string SourceDirectory = "";
+    static string projectName;
+    static string ProjectName => projectName ??
+        (projectName = new FileInfo(Application.dataPath).Directory.Name);
+    static string PluginName = "Plugin Name";
+    static string PluginVersion = "1.0.0";
+    static string SourceDirectory = "";
 
     [MenuItem("URack/Export Plugin")]
     static void Open()
     {
-        var window = GetWindowWithRect<ExporterWindow>(new Rect(0, 0, 500, 300));
+        var window = GetWindowWithRect<ExporterWindow>(new Rect(0, 0, 220, 200));
         window.Show();
-        window.minSize = new Vector2(200, 200);
-        window.maxSize = new Vector2(700, 700);
+        window.minSize = new Vector2(220, 200);
         window.titleContent = new GUIContent("Export URack Plugin");
+        if (PlayerPrefs.HasKey("URackExporter_PluginName_" + ProjectName))
+        {
+            PluginName = PlayerPrefs.GetString("URackExporter_PluginName_" + ProjectName);
+            PluginVersion = PlayerPrefs.GetString("URackExporter_PluginVersion_" + ProjectName);
+            SourceDirectory = PlayerPrefs.GetString("URackExporter_SourceDirectory_" + ProjectName);
+        }
     }
 
     void OnGUI()
     {
+        GUILayout.BeginHorizontal();
+        GUILayout.FlexibleSpace();
         GUILayout.BeginVertical();
         EditorGUILayout.LabelField("Plugin name");
         PluginName = EditorGUILayout.TextField(PluginName);
+        EditorGUILayout.LabelField("Plugin version");
+        PluginVersion = EditorGUILayout.TextField(PluginVersion);
         EditorGUILayout.LabelField("Source directory");
         EditorGUILayout.LabelField("(leave blank to use 'Assets' folder)");
         SourceDirectory = EditorGUILayout.TextField(SourceDirectory);
+        EditorGUILayout.Space();
         if (GUILayout.Button("Export"))
             Build();
+        GUILayout.EndVertical();
+        GUILayout.FlexibleSpace();
+        GUILayout.EndHorizontal();
     }
 
     void Build()
     {
         var pluginName = PluginName.Replace(" ", "");
 
-        var parentDir = EditorUtility
-            .OpenFolderPanel("Select Output Directory", Application.dataPath.Replace("/Assets", ""), "Build");
+        var parentDir = EditorUtility.OpenFolderPanel("Select Output Directory",
+            Application.dataPath.Replace("/Assets", ""), "Build");
 
+        // exit if dialog cancelled
+        if (parentDir == "") return;
+
+        // create temporary output directory
         var outputDirPath = parentDir + "/" + pluginName;
-        var outputDir = new FileInfo(outputDirPath).Directory;
+        var outputDir = new DirectoryInfo(outputDirPath);
         outputDir.Create();
         var createDirectoryTimeout = 0;
         while (!outputDir.Exists && createDirectoryTimeout < 100)
@@ -59,37 +81,49 @@ public class ExporterWindow : EditorWindow
         var compilerArgs = "-t:library -out:" + pluginDll;
 
         // reference UnityEngine libs and package dlls used in current project
-        var editorPath = EditorApplication.applicationPath.Replace("Unity.exe", "");
-        var unityLibsPath = editorPath + "Data/Managed/UnityEngine/";
-        var packageLibPath = Application.dataPath.Replace("Assets", "Library/ScriptAssemblies/");
+        var editorPath = Path.GetDirectoryName(EditorApplication.applicationPath);
+
+        var unityLibsPath = editorPath + "/Data/Managed/UnityEngine/";
+        var packageLibPath = Application.dataPath.Replace("Assets", "/Library/ScriptAssemblies/");
 
         compilerArgs += " -lib:\"" + unityLibsPath + "\"" + ",\"" + packageLibPath + "\"";
 
-        foreach(var unityLib in Directory.GetFiles(unityLibsPath))
+        foreach (var unityLib in Directory.GetFiles(unityLibsPath))
             if (unityLib.Contains(".dll"))
                 compilerArgs += " -r:\"" + Path.GetFileName(unityLib) + "\"";
 
-        foreach(var packageLib in Directory.GetFiles(packageLibPath))
+        foreach (var packageLib in Directory.GetFiles(packageLibPath))
             if (packageLib.Contains(".dll") && !packageLib.Contains("Editor"))
                 compilerArgs += " -r:\"" + Path.GetFileName(packageLib) + "\"";
 
         // add project source files
         compilerArgs += " -recurse:" + Application.dataPath + "/" + SourceDirectory + "/*.cs ";
 
+        // TODO since mono comes bundled with unity, figure out how
+        // to use this version instead of the one on Path
+        // var compilerPath = EditorApplication.applicationPath.Replace("Unity.exe", "");
+        // compilerPath += "Data/MonoBleedingEdge/lib/mono/4.5/mcs.exe";
+
         // compile plugin dll
         Process.Start("mcs", compilerArgs).WaitForExit();
 
-        // copy resources to output folder
-        var assetsFolder = new DirectoryInfo(Application.dataPath + "/" + SourceDirectory + "/");
-        foreach(var resource in assetsFolder.GetFiles("*.*", SearchOption.AllDirectories))
-        if (resource.FullName.Contains("\\Resources\\") || resource.FullName.Contains("/Resources/"))
-            if (!resource.Name.Contains(".meta") && resource.Name.Contains("."))
-            {
-                var newPath = outputDirPath + "/Resources/" + resource.Name;
-                new FileInfo(newPath).Directory.Create();
-                File.Copy(resource.FullName, newPath, true);
-            }
+        // build and export asset bundles
+        BuildPipeline.BuildAssetBundles(outputDirPath,
+            BuildAssetBundleOptions.None, EditorUserBuildSettings.activeBuildTarget);
 
-        EditorUtility.RevealInFinder(pluginDll);
+        // compress all files into a URack plugin archive
+        var pluginArchive = parentDir + "/" + pluginName + "-" + PluginVersion + ".zip";
+        if (File.Exists(pluginArchive)) File.Delete(pluginArchive);
+        Archiver.Compress(outputDirPath, pluginArchive);
+
+        // remove temp directory now it's archived
+        Directory.Delete(outputDirPath, true);
+
+        EditorUtility.RevealInFinder(pluginArchive);
+
+        // store plugin export settings for next export
+        PlayerPrefs.SetString("URackExporter_PluginName_" + ProjectName, PluginName);
+        PlayerPrefs.SetString("URackExporter_PluginVersion_" + ProjectName, PluginVersion);
+        PlayerPrefs.SetString("URackExporter_SourceDirectory_" + ProjectName, SourceDirectory);
     }
 }
--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # URack-Unity
 
-* Thanks [pardeike](https://github.com/pardeike) for the [Harmony](https://github.com/pardeike/Harmony) library, which is used to patch methods and properties at runtime, making life easier for anyone developing modules.
+* Thanks [pardeike](https://github.com/pardeike) for the [Harmony](https://github.com/pardeike/Harmony) library, which is used to patch methods and properties at runtime.
 It is licensed under the MIT license.
 
-* The OSC message encoding and parsing is based on [this implementation](https://github.com/keijiro/unity-osc).
-Thanks [keijiro](https://github.com/keijiro)!
-It is also licensed under the MIT license.
+* The OSC message encoding and parsing is based on [this implementation](https://github.com/keijiro/unity-osc) by [keijiro](https://github.com/keijiro).
+It is licensed under the MIT license.
+
+* Packing and unpacking URack plugins uses the [Archiver-Unity](https://github.com/LightBuzz/Archiver-Unity) project by [Vangos Pternias](http://pterneas.com/) from [LightBuzz](http://lightbuzz.com/).
+It is licensed under the Apache License 2.0.
